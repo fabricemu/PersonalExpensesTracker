92e8025bdd9b9a10980cc42b53572fab7a2eb411
fabricemu <fabthemachine@gmail.com>
2024-01-30 00:23:32 +0200

    Fix form modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
 const loginModal = document.getElementById("loginModal");
 const signupModal = document.getElementById("signupModal");
-const loginBtn = document.querySelector(".login-btn");
-const signupBtn = document.querySelector(".signup-btn");
+const loginBtn = document.getElementsByClassName("login-btn");
+const signupBtn = document.getElementsByClassName("signup-btn");
 const closeBtns = document.getElementsByClassName("close");
-loginBtn.onclick = () => {
-  loginModal.style.display = "block";
+for (const j of loginBtn) {
+    j.onclick = () => {
+        signupModal.style.display = "none";
+        loginModal.style.display = "block";
+    }
 }
-signupBtn.onclick = () => {
-  signupModal.style.display = "block";
+// loginBtn.onclick = () => {
+//     loginModal.style.display = "block";
+// }
+for (const i of signupBtn) {
+    i.onclick = () => {
+        loginModal.style.display = "none";
+        signupModal.style.display = "block";
+    }
 }
 for (const closeBtn of closeBtns) {
-  closeBtn.onclick = () => {
-    loginModal.style.display = "none";
-    signupModal.style.display = "none";
-  }
+    closeBtn.onclick = () => {
+        loginModal.style.display = "none";
+        signupModal.style.display = "none";
+    }
 }
 window.onclick = (event) => {
-  if (event.target === loginModal || event.target === signupModal) {
-    loginModal.style.display = "none";
-    signupModal.style.display = "none";
-  }
+    if (event.target === loginModal || event.target === signupModal) {
+        loginModal.style.display = "none";
+        signupModal.style.display = "none";
+    }
 }
